876f3bef81b47bb38dde4f2937a6d6192b72586e
Ayush Shrestha <Ayushrestha05@users.noreply.github.com>
2020-09-08 22:31:13 +0545

    Designer Door Mat Complete

--- Designer Door Mat.py
+++ Designer Door Mat.py
@@ -1,5 +1,18 @@
+def design(N, M):
+    for i in range(1, N, 2):
+        print(((M - 3 * i) // 2) * '-' + i * '.|.' + ((M - 3 * i) // 2) * '-')
+    print(((M - 7) // 2) * '-' + 'WELCOME' + ((M - 7) // 2) * '-')
+    for i in range(N - 2, -1, -2):
+        print(((M - 3 * i) // 2) * '-' + i * '.|.' + ((M - 3 * i) // 2) * '-')
+    for i in range(1, n, 1):
+        for j in range(1, m, 1):
+            print('-')
+    print('\n')
+
+
 if __name__ == '__main__':
     userInput = input()
     split = userInput.split(' ')
     n = int(split[0])
     m = int(split[1])
+    design(n, m)
